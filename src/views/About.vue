a8c2c252615072248e49ff206321fe9210e1c8f4
Stkotn <166301507+Stkotn@users.noreply.github.com>
2025-09-21 12:53:01 +0200

    2025/09/21 4th

--- src/views/About.vue
+++ src/views/About.vue
@@ -13,6 +13,6 @@ import profileImg from '@/assets/images/profile.jpg'
     <p class="text-text-primary mb-2">愛知県豊橋市生まれ。北マケドニア在住。</p>
     <p class="text-text-primary mb-2">愛知大学を卒業後、地元の信用金庫で窓口業務を2年間担当。その後マルタ島へ語学留学し、現地企業でカスタマーサポートとして勤務。</p>
     <p class="text-text-primary mb-2">そのまま海外でキャリアチェンジをし、Webライターとして2年半にわたりコンテンツマーケティングに従事。Webコンテンツ制作、SEO記事、SNS運用、バナーやLPのライティングなどを担当する。</p>
-    <p class="text-text-primary mb-2">現在は夫の母国である北マケドニア・オフリドを拠点に、ブリッジSEとしてWeb開発チームに携わっている。</p>
+    <p class="text-text-primary mb-2">現在は夫の母国である北マケドニアを拠点に、ブリッジSEとしてWeb開発チームに携わっている。</p>
   </section>
 </template>
